5b08b77613ea1e96ecf57765f48adeceb3d801a6
Mohamed Adam <35684384+modeek@users.noreply.github.com>
2021-12-18 17:03:10 -0800

    Update postgresql@9.6.rb

--- Formula/postgresql@9.6.rb
+++ Formula/postgresql@9.6.rb
@@ -17,9 +17,6 @@ class PostgresqlAT96 < Formula
 
   keg_only :versioned_formula
 
-  # https://www.postgresql.org/support/versioning/
-  disable! date: "2021-11-11", because: :unsupported
-
   depends_on "openssl@1.1"
   depends_on "readline"
 
